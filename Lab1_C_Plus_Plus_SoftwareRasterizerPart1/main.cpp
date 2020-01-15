45bfb483ce8687d30ec49598613d64ff1783dc93
nicolassims <sableye.nico@gmail.com>
2020-01-14 23:23:46 -0500

    Got triangles being drawn.

--- Lab1_C_Plus_Plus_SoftwareRasterizerPart1/main.cpp
+++ Lab1_C_Plus_Plus_SoftwareRasterizerPart1/main.cpp
@@ -61,6 +61,12 @@ void drawLine(Vec2 v0, Vec2 v1, TGA& image, ColorRGB c){
     }
 }
 
+void swap(Vec2 &v1, Vec2 &v0) {
+	Vec2 holder = v1;
+	v1 = v0;
+	v0 = holder;
+}
+
 float sign(Vec2 p1, Vec2 p2, Vec2 p3) {
 	return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
 }
@@ -68,8 +74,6 @@ float sign(Vec2 p1, Vec2 p2, Vec2 p3) {
 bool PointInTriangle(Vec2 pt, Vec2 v1, Vec2 v2, Vec2 v3) {
 	float d1 = sign(pt, v1, v2), d2 = sign(pt, v2, v3), d3 = sign(pt, v3, v1);
 	bool has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0), has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
-	float d1, d2, d3;
-	bool has_neg, has_pos;
 
 	d1 = sign(pt, v1, v2);
 	d2 = sign(pt, v2, v3);
@@ -83,7 +87,7 @@ bool PointInTriangle(Vec2 pt, Vec2 v1, Vec2 v2, Vec2 v3) {
 
 // Draw a triangle
 void triangle(Vec2 v0, Vec2 v1, Vec2 v2, TGA &image, ColorRGB c){
-    if (glFillMode==LINE){
+    if(glFillMode==LINE){
         drawLine(v0,v1,image,c);
         drawLine(v1,v2,image,c);
         drawLine(v2,v0,image,c);
@@ -106,7 +110,6 @@ void triangle(Vec2 v0, Vec2 v1, Vec2 v2, TGA &image, ColorRGB c){
 			}
 		}
 	}
-
 		//FIX THIS
 		std::cout << v0.x << "," << v0.y << "," << v1.x << "," << v1.y << "," << v2.x << "," << v2.y << std::endl;
 
@@ -117,19 +120,16 @@ void triangle(Vec2 v0, Vec2 v1, Vec2 v2, TGA &image, ColorRGB c){
 		//FIX THIS
 		std::cout << v0.x << "," << v0.y << "," << v1.x << "," << v1.y << "," << v2.x << "," << v2.y << std::endl;
 
-		int counter = 0;
 		for (int i = minx; i < maxx; i++) {
-			bool columnstarted = false;
 			for (int j = miny; j < maxy; j++) {
-				counter++;
 				if (PointInTriangle(Vec2(i,j), v0, v1, v2)) {
-					columnstarted = true;
 					canvas.setPixelColor(i, j, c);
-				} else if (columnstarted) {
-					break;
 				}
 			}
 		}
+
+		//FIX THIS
+		std::cout << std::to_string(minx) << "," << std::to_string(maxx) << "," << std::to_string(miny) << "," << std::to_string(maxy) << ",";
 	}
 }
 
@@ -182,6 +182,24 @@ int main(){
 	triangle(tri[0], tri[1], tri[2], canvas, red);
 	triangle(tri2[0], tri2[1], tri2[2], canvas, green);
 	triangle(tri1[0], tri1[1], tri1[2], canvas, blue);
+
+	// Points for our Line
+	Vec2 line[2] = { Vec2(0,0), Vec2(100,100) };
+
+	// Set the fill mode
+	glPolygonMode(FILL);
+
+	// Draw a line
+	drawLine(line[0], line[1], canvas, red);
+
+	// Data for our triangle
+	Vec2 tri[3] = { Vec2(50,190),Vec2(190,100),Vec2(100,50) };
+
+	//Draws the outline of the triangle. //FIX THIS
+	glFillMode = FILL;
+
+	// Draw a triangle
+	triangle(tri[0], tri[1], tri[2], canvas, red);
 
 		// Output the final image
 	canvas.outputTGAImage("graphics_lab2.ppm");
