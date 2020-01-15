51677b5854c7d60e1f33b3561b649d7991c6ad85
nicolassims <sableye.nico@gmail.com>
2020-01-14 23:57:06 -0500

    Created application that shows three different colored triangles.

--- Lab1_C_Plus_Plus_SoftwareRasterizerPart1/main.cpp
+++ Lab1_C_Plus_Plus_SoftwareRasterizerPart1/main.cpp
@@ -72,22 +72,14 @@ float sign(Vec2 p1, Vec2 p2, Vec2 p3) {
 }
 
 bool PointInTriangle(Vec2 pt, Vec2 v1, Vec2 v2, Vec2 v3) {
-	float d1, d2, d3;
-	bool has_neg, has_pos;
-
-	d1 = sign(pt, v1, v2);
-	d2 = sign(pt, v2, v3);
-	d3 = sign(pt, v3, v1);
-
-	has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
-	has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
-
+	float d1 = sign(pt, v1, v2), d2 = sign(pt, v2, v3), d3 = sign(pt, v3, v1);
+	bool has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0), has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
 	return !(has_neg && has_pos);
 }
 
 // Draw a triangle
 void triangle(Vec2 v0, Vec2 v1, Vec2 v2, TGA &image, ColorRGB c){
-    if(glFillMode==LINE){
+    if (glFillMode==LINE){
         drawLine(v0,v1,image,c);
         drawLine(v1,v2,image,c);
         drawLine(v2,v0,image,c);
@@ -96,17 +88,6 @@ void triangle(Vec2 v0, Vec2 v1, Vec2 v2, TGA &image, ColorRGB c){
 		int maxx = std::max(v0.x, std::max(v1.x, v2.x));
 		int miny = std::min(v0.y, std::min(v1.y, v2.y));
 		int maxy = std::max(v0.y, std::max(v1.y, v2.y));
-
-		//FIX THIS
-		std::cout << v0.x << "," << v0.y << "," << v1.x << "," << v1.y << "," << v2.x << "," << v2.y << std::endl;
-
-		//if (v1.y < v0.y) { swap(v1, v0); }
-		//if (v2.y < v0.y) { swap(v2, v0); }
-		//if (v2.y < v1.y) { swap(v2, v1); }
-
-		//FIX THIS
-		std::cout << v0.x << "," << v0.y << "," << v1.x << "," << v1.y << "," << v2.x << "," << v2.y << std::endl;
-
 		for (int i = minx; i < maxx; i++) {
 			for (int j = miny; j < maxy; j++) {
 				if (PointInTriangle(Vec2(i,j), v0, v1, v2)) {
@@ -114,20 +95,16 @@ void triangle(Vec2 v0, Vec2 v1, Vec2 v2, TGA &image, ColorRGB c){
 				}
 			}
 		}
-
-		//FIX THIS
-		std::cout << std::to_string(minx) << "," << std::to_string(maxx) << "," << std::to_string(miny) << "," << std::to_string(maxy) << ",";
 	}
-
-    // TODO: Draw a filled triangle
 }
 
 // Main
 int main(){
-
     // A sample of color(s) to play with
-    ColorRGB red;
+    ColorRGB red, blue, green;
     red.r = 255; red.g = 0; red.b = 0;
+	blue.r = 0; blue.g = 0; blue.b = 255;
+	green.r = 0; green.g = 255; green.b = 0;
 
 	// Points for our Line
 	Vec2 line[2] = { Vec2(0,0), Vec2(100,100) };
@@ -139,13 +116,16 @@ int main(){
 	drawLine(line[0], line[1], canvas, red);
 
 	// Data for our triangle
-	Vec2 tri[3] = { Vec2(50,190),Vec2(190,100),Vec2(100,50) };
+	Vec2 tri[3] = { Vec2(0,320), Vec2(320,320), Vec2(160,0) };
+	Vec2 tri1[3] = { Vec2(160,320), Vec2(0,160), Vec2(320,160) };
+	Vec2 tri2[3] = { Vec2(11,213), Vec2(213,312), Vec2(312,11) };
 
-	//Draws the outline of the triangle. //FIX THIS
 	glFillMode = FILL;
 
 	// Draw a triangle
 	triangle(tri[0], tri[1], tri[2], canvas, red);
+	triangle(tri2[0], tri2[1], tri2[2], canvas, green);
+	triangle(tri1[0], tri1[1], tri1[2], canvas, blue);
 
 		// Output the final image
 	canvas.outputTGAImage("graphics_lab2.ppm");
