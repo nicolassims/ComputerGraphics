8d4024a818f7e0a20001e0944f2b8ba9d4da4869
nicolassims <sableye.nico@gmail.com>
2020-01-20 17:58:08 -0500

    Implemented ppm.cpp. I think.

--- Assignment0_CPlusPlus/src/main.cpp
+++ Assignment0_CPlusPlus/src/main.cpp
@@ -6,15 +6,11 @@
 int main(){
 
 #include "../include/PPM.h"
-#include <iostream>//FIX THIS
 
 int main(){
-
     PPM myPPM("../textures/test1.ppm");
     myPPM.darken();
     myPPM.savePPM("../textures/test1_darken.ppm");
 
-	int dummy;
-	std::cin >> dummy;
     return 0;
 }
