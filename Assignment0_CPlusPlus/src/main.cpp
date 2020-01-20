2c01103656e2a163f85adefcb58c75c53997b979
nicolassims <sableye.nico@gmail.com>
2020-01-20 17:58:08 -0500

    Implemented ppm.cpp. I think.

---
+++ Assignment0_CPlusPlus/src/main.cpp
@@ -0,0 +1,10 @@
+// Include our custom library
+#include "../include/PPM.h"
+
+int main(){
+    PPM myPPM("../textures/test1.ppm");
+    myPPM.darken();
+    myPPM.savePPM("../textures/test1_darken.ppm");
+
+    return 0;
+}
