84c756aab9214feea1a01782ec16b17fbaa375c9
nicolassims <sableye.nico@gmail.com>
2020-01-20 02:17:32 -0500

    Finished implementing constructor, code-wise. Still requires refinements.

--- Assignment0_CPlusPlus/src/ppm.cpp
+++ Assignment0_CPlusPlus/src/ppm.cpp
@@ -4,12 +4,32 @@
 #include <vector>
 #include <algorithm>
 
+#include <cstring>
+#include <sstream> 
+#include <array>
+#include <iostream>//FIX THIS
+
 // Constructor loads a filename with the .ppm extension
 PPM::PPM(std::string fileName) {
 	// Opens a file to output
 	std::ifstream inFile;
 
 	// Now we can open our file.
+	// You will notice the name is 'ifstream'
+	// This means 'input file stream'
+	std::ifstream inFile;
+
+	// Now we are going to read in some data.
+	// I want to actually make our program more flexible
+	// and read in a file from the command line.
+	// To do this, we'll use the argc, and argv**
+	// arguments of our main function.
+	// So when we run our program, we will type on the command-line:
+	// ./input objects/bunny.obj"
+
+	// Now we can open our file.
+	// Think about why this is argv[1].
+	// Try printing it out to see what this is.
 	inFile.open(fileName);
 
 	// Lets also check to make sure the filepath is correct.
@@ -47,6 +67,27 @@ PPM::PPM(std::string fileName) {
 					//Handle whatever value the PPM is currently scaled to
 					} else if (headerLines == 0) {
 						maxValue = std::stod(line);
+		std::cout << "opened" << std::endl;
+		// Now we can do some work
+		// We'll learn about strings in a moment,
+		// but we are using getline to read in
+		// one string of text at a time, until
+		// we have nothing left to read.
+		int headerLines = 2;
+		std::string line;   // A string object.
+		std::vector<unsigned char> coordinates;
+		while (getline(inFile, line)) {
+			if (line.substr(0, 1) != "#") {
+				if (headerLines == 0) {
+					coordinates.push_back((unsigned char)std::stoi(line));
+				}
+				else {
+					headerLines--;
+					if (headerLines == 0) {
+						int space = line.find(" ");
+						m_width = std::stoi(line.substr(0, space));
+						m_height = std::stoi(line.substr(space, line.length()));
+						//std::cout << m_width << " " << m_height << std::endl;
 					}
 				}
 			}
@@ -64,6 +105,10 @@ PPM::PPM(std::string fileName) {
 		coordinates.erase(coordinates.begin(), coordinates.end());
 	}
 	//Close the file we're reading from
+		m_PixelData = &coordinates.front();
+	}
+
+	// Closes the file we are reading from
 	inFile.close();
 }
 
@@ -99,6 +144,15 @@ void PPM::darken() {
 		//Decrease the value of a cell's RGB values by 50, or set it to zero, if it's already below 50.
 		m_PixelData[i] = std::max(m_PixelData[i] - 50, 0);
 	}
+    // TODO: Save a PPM image to disk
+}
+
+// Darken subtracts 50 from each of the red, green
+// and blue color components of all of the pixels
+// in the PPM. Note that no values may be less than
+// 0 in a ppm.
+void PPM::darken() {
+    // TODO: Output a 'filtered' PPM image.
 }
 
 // Sets a pixel to a specific R,G,B value 
