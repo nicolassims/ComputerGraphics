37610c639f7fdc2daaf9607e412e22ee0dd51b33
nicolassims <sableye.nico@gmail.com>
2020-01-20 02:17:32 -0500

    Finished implementing constructor, code-wise. Still requires refinements.

--- Assignment0_CPlusPlus/src/main.cpp
+++ Assignment0_CPlusPlus/src/main.cpp
@@ -1,11 +1,14 @@
 // Include our custom library
-#include "PPM.h"
+#include "../include/PPM.h"
+#include <iostream>//FIX THIS
 
 int main(){
 
-    PPM myPPM("./textures/test1.ppm");
+    PPM myPPM("../textures/test1.ppm");
     myPPM.darken();
-    myPPM.savePPM("./textures/test1_darken.ppm");
+    myPPM.savePPM("../textures/test1_darken.ppm");
 
+	int dummy;
+	std::cin >> dummy;
     return 0;
 }
--- Assignment0_CPlusPlus/src/ppm.cpp
+++ Assignment0_CPlusPlus/src/ppm.cpp
@@ -1,17 +1,74 @@
-#include "PPM.h"
+#include "../include/PPM.h"
+#include <fstream>
+#include <string> // new library!
+#include <vector>
+#include <cstring>
+#include <sstream> 
+#include <array>
+#include <iostream>//FIX THIS
+
 
 // Constructor loads a filename with the .ppm extension
-PPM::PPM(std::string fileName){
-    // TODO:    Load and parse a ppm to get its pixel
-    //          data stored properly.
+PPM::PPM(std::string fileName) {
+	// Opens a file to output
+	// You will notice the name is 'ifstream'
+	// This means 'input file stream'
+	std::ifstream inFile;
+
+	// Now we are going to read in some data.
+	// I want to actually make our program more flexible
+	// and read in a file from the command line.
+	// To do this, we'll use the argc, and argv**
+	// arguments of our main function.
+	// So when we run our program, we will type on the command-line:
+	// ./input objects/bunny.obj"
+
+	// Now we can open our file.
+	// Think about why this is argv[1].
+	// Try printing it out to see what this is.
+	inFile.open(fileName);
+
+	// Lets also check to make sure the filepath is correct.
+	if (inFile.is_open()) {
+		std::cout << "opened" << std::endl;
+		// Now we can do some work
+		// We'll learn about strings in a moment,
+		// but we are using getline to read in
+		// one string of text at a time, until
+		// we have nothing left to read.
+		int headerLines = 2;
+		std::string line;   // A string object.
+		std::vector<unsigned char> coordinates;
+		while (getline(inFile, line)) {
+			if (line.substr(0, 1) != "#") {
+				if (headerLines == 0) {
+					coordinates.push_back((unsigned char)std::stoi(line));
+				}
+				else {
+					headerLines--;
+					if (headerLines == 0) {
+						int space = line.find(" ");
+						m_width = std::stoi(line.substr(0, space));
+						m_height = std::stoi(line.substr(space, line.length()));
+						//std::cout << m_width << " " << m_height << std::endl;
+					}
+				}
+			}
+		}
+
+		m_PixelData = &coordinates.front();
+	}
+
+	// Closes the file we are reading from
+	inFile.close();
 }
 
 // Destructor clears any memory that has been allocated
-PPM::~PPM(){
+PPM::~PPM() {
 }
 
 // Saves a PPM Image to a new file.
-void PPM::savePPM(std::string outputFileName){
+void PPM::savePPM(std::string outputFileName) {
     // TODO: Save a PPM image to disk
 }
 
@@ -19,11 +76,11 @@ void PPM::savePPM(std::string outputFileName){
 // and blue color components of all of the pixels
 // in the PPM. Note that no values may be less than
 // 0 in a ppm.
-void PPM::darken(){
+void PPM::darken() {
     // TODO: Output a 'filtered' PPM image.
 }
 
 // Sets a pixel to a specific R,G,B value 
-void PPM::setPixel(int x, int y, int R, int G, int B){
+void PPM::setPixel(int x, int y, int R, int G, int B) {
     // TODO: Implement
 }
