54c92a2d75b67baf1aedab266a2dd37190d80583
nicolassims <sableye.nico@gmail.com>
2020-01-20 02:17:32 -0500

    Finished implementing constructor, code-wise. Still requires refinements.

--- Assignment0_CPlusPlus/src/main.cpp
+++ Assignment0_CPlusPlus/src/main.cpp
@@ -1,11 +1,20 @@
 // Include our custom library
+
 //Compile by typing "g++ ppm.cpp main.cpp -o main"
 #include "../include/PPM.h"
 
 int main(){
+
+#include "../include/PPM.h"
+#include <iostream>//FIX THIS
+
+int main(){
+
     PPM myPPM("../textures/test1.ppm");
     myPPM.darken();
     myPPM.savePPM("../textures/test1_darken.ppm");
 
+	int dummy;
+	std::cin >> dummy;
     return 0;
 }
