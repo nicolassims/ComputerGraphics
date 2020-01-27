89613a90088a6e298b440600150cb22ce7add707
whackney <hackney.w@husky.neu.edu>
2020-01-27 10:13:46 -0500

    pushed all of the lab3 work

---
+++ Lab3_Starfield/build/Lab2.build/Debug/Lab2.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/bhackney/Documents/Computer\ Graphics/Computer-Graphics-work/Lab3_Starfield/main.cpp \
+  /Users/bhackney/Documents/Computer\ Graphics/Computer-Graphics-work/Lab3_Starfield/Lab2.h
