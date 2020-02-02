b11776f9e515e6c0abe5162952e23214fbade11d
nicolassims <sableye.nico@gmail.com>
2020-02-02 13:29:23 -0500

    Sorted constructor order, and fully implmeneted identity function.

--- Assignment1_MathFoundations1/include/Matrix4f.h
+++ Assignment1_MathFoundations1/include/Matrix4f.h
@@ -17,17 +17,16 @@ private:
 public:
     Matrix4f() = default;
 
-    // TODO: Row or column major order you decide!
     // Matrix constructor with 9 scalar values.
     Matrix4f( float n00, float n01, float n02, float n03,
               float n10, float n11, float n12, float n13,
               float n20, float n21, float n22, float n23,
               float n30, float n31, float n32, float n33){
 
-        n[0][0] = n00; n[0][1] = n10; n[0][2] = n20; n[0][3] = n30;
-        n[1][0] = n01; n[1][1] = n11; n[1][2] = n21; n[1][3] = n31;
-        n[2][0] = n02; n[2][1] = n12; n[2][2] = n22; n[2][3] = n32;
-        n[3][0] = n03; n[3][1] = n13; n[3][2] = n23; n[3][3] = n33;
+        n[0][0] = n00; n[0][1] = n01; n[0][2] = n02; n[0][3] = n03;
+        n[1][0] = n10; n[1][1] = n11; n[1][2] = n12; n[1][3] = n13;
+        n[2][0] = n20; n[2][1] = n21; n[2][2] = n22; n[2][3] = n23;
+        n[3][0] = n30; n[3][1] = n31; n[3][2] = n32; n[3][3] = n33;
     }
 
     // Matrix constructor from four vectors.
@@ -41,7 +40,10 @@ public:
 
     // Makes the matrix an identity matrix
     void identity(){
-        n[0][0] = 1.0f;
+		n[0][0] = 1; n[0][1] = 0; n[0][2] = 0; n[0][3] = 0;
+		n[1][0] = 0; n[1][1] = 1; n[1][2] = 0; n[1][3] = 0;
+		n[2][0] = 0; n[2][1] = 0; n[2][2] = 1; n[2][3] = 0;
+		n[3][0] = 0; n[3][1] = 0; n[3][2] = 0; n[3][3] = 1;
     }
 
     // Index operator with two dimensions
