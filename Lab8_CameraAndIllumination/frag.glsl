9d40aa4cb8074934b8ff6ff4b61e3aaa60204fb0
nicolassims <sableye.nico@gmail.com>
2020-03-10 20:33:42 -0400

    Implemented updated vertex shader. Picture still showing for now.

--- Lab8_CameraAndIllumination/frag.glsl
+++ Lab8_CameraAndIllumination/frag.glsl
@@ -31,61 +31,10 @@ uniform sampler2D tex;              // our primary texture
 uniform mat4 view;                  // we need the view matrix for highlights
 uniform PointLight pointLights[1];  // Our lights
 
-// Used for our specular highlights
-uniform mat4 view;
-
-
-// Import our normal data
-in vec3 myNormal;
-// Import our texture coordinates from vertex shader
-in vec2 v_texCoord;
-// Import the fragment position
-in vec3 FragPos;
-
-// If we have texture coordinates, they are stored in this sampler.
-uniform sampler2D u_DiffuseMap;
-
 void main() {
-// Compute the normal direction
-  vec3 norm = normalize(myNormal);
+  // Set our output fragment color to whatever we pull from our input texture (Note, change 'tex' to whatever the sampler is named)
+  fragColor = texture(tex, texCoords);
 
-  // Store our final texture color
-  vec3 diffuseColor;
-  diffuseColor = texture(u_DiffuseMap, v_texCoord).rgb;
-
-  // (1) Compute ambient light
-  vec3 ambient = pointLights[0].ambientIntensity * pointLights[0].color;
-
-  // (2) Compute diffuse light
-  // From our lights position and the fragment, we can get
-  // a vector indicating direction
-  // Note it is always good to 'normalize' values.
-  vec3 lightDir = normalize(pointLights[0].position - FragPos);
-  // Now we can compute the diffuse light impact
-  float diffImpact = max(dot(norm, lightDir), 0.0);
-  vec3 diffuseLight = diffImpact * pointLights[0].color;
-
-  // (3) Compute Specular lighting
-  vec3 viewPos = vec3(0.0,0.0,0.0);
-  vec3 viewDir = normalize(viewPos - FragPos);
-  vec3 reflectDir = reflect(-lightDir, norm);
-
-  float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-  vec3 specular = pointLights[0].specularStrength * spec * pointLights[0].color;
-
-  // Calculate Attenuation here
-  // distance and lighting...
-
-  // Our final color is now based on the texture.
-  // That is set by the diffuseColor
-  vec3 Lighting = diffuseLight + ambient + specular;
-
-  // Final color + "how dark or light to make fragment"
-  if(gl_FrontFacing){
-      FragColor = vec4(diffuseColor * Lighting,1.0);
-  }else{
-      // Additionally color the back side the same color
-       FragColor = vec4(diffuseColor * Lighting,1.0);
-  }
-
+  // TODO:  Implement some form of lighting.
+  
 }
--- Lab8_CameraAndIllumination/vert.glsl
+++ Lab8_CameraAndIllumination/vert.glsl
@@ -1,37 +1,27 @@
-#version 330 core
-// Read in our attributes stored from our vertex buffer object
-// We explicitly state which is the vertex information
-// (The first 3 floats are positional data, we are putting in our vector)
-layout(location=0)in vec3 position; 
-layout(location=1)in vec3 normals; // Our second attribute - normals.
-layout(location=2)in vec2 texCoord; // Our third attribute - texture coordinates.
-layout(location=3)in vec3 tangents; // Our third attribute - texture coordinates.
-layout(location=4)in vec3 bitangents; // Our third attribute - texture coordinates.
+#version 330
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec3 normal;
+layout(location = 2) in vec2 textureCoords;
 
-// If we are applying our camera, then we need to add some uniforms.
-// Note that the syntax nicely matches glm's mat4!
-uniform mat4 model; // Object space
-uniform mat4 view; // Object space
-uniform mat4 projection; // Object space
+// We now have our camera system set up.
+uniform mat4 modelMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
 
-// Export our normal data, and read it into our frag shader
-out vec3 myNormal;
-// Export our Fragment Position computed in world space
-out vec3 FragPos;
-// If we have texture coordinates we can now use this as well
-out vec2 v_texCoord;
-
+// We define a new output vec2 for our texture coorinates.
+out vec2 texCoords;
+out vec3 norm;
+out vec3 fragPos;
 
 void main()
 {
-
-    gl_Position = projection * view * model * vec4(position, 1.0f);
-
-    myNormal = normals;
-    // Transform normal into world space
-    FragPos = vec3(model* vec4(position,1.0f));
-
-    // Store the texture coordinatests which we will output to
-    // the next stage in the graphics pipeline.
-    v_texCoord = texCoord;
+    // We have our transformed position set properly now
+    gl_Position = projectionMatrix*viewMatrix*modelMatrix*vec4(position, 1.0);
+    // Our fragment pos for lighting.
+    fragPos = (modelMatrix*vec4(position, 1.0)).xyz;
+    // Make sure to transform the normal
+    vec4 tmpnorm = modelMatrix*vec4(normal, 0.0);
+    norm = normalize(tmpnorm.xyz);
+    // And we map our texture coordinates as appropriate
+    texCoords = textureCoords;
 }
