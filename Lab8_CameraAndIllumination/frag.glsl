6698ae6f7a1e9de6ce51418dc230083c7e1a6a28
whackney <hackney.w@husky.neu.edu>
2020-03-10 20:27:21 -0400

    updated shader with correct names

--- Lab8_CameraAndIllumination/frag.glsl
+++ Lab8_CameraAndIllumination/frag.glsl
@@ -1,4 +1,4 @@
-#version 330 core
+#version 330
 
 // Take in our texture coordinate from our vertex shader
 in vec2 texCoords;
@@ -7,16 +7,15 @@ in vec3 norm;
 // And our fragment position for lighting
 in vec3 fragPos;
 
-// The final output color of each 'fragment' from our fragment shader.
-out vec4 FragColor;
+// We always define a fragment color that we output.
+out vec4 fragColor;
 
-// Our light source data structure
-struct PointLight{
-    vec3 lightColor;
-    vec3 lightPos;
+// Define our light(s)
+struct PointLight {
+    vec3 color;
+    vec3 position;
     float ambientIntensity;
-
-    float specularStrength;
+    float specularIntensity;
 
     float constant;
     float linear;
@@ -27,8 +26,10 @@ struct PointLight{
     vec3 specular;
 };
 
-uniform PointLight pointLights[1];
-
+// Maintain our uniforms.
+uniform sampler2D tex;              // our primary texture
+uniform mat4 view;                  // we need the view matrix for highlights
+uniform PointLight pointLights[1];  // Our lights
 
 // Used for our specular highlights
 uniform mat4 view;
@@ -44,47 +45,47 @@ in vec3 FragPos;
 // If we have texture coordinates, they are stored in this sampler.
 uniform sampler2D u_DiffuseMap;
 
-void main()
-{
-    // Compute the normal direction
-    vec3 norm = normalize(myNormal);
+void main() {
+// Compute the normal direction
+  vec3 norm = normalize(myNormal);
 
-    // Store our final texture color
-    vec3 diffuseColor;
-    diffuseColor = texture(u_DiffuseMap, v_texCoord).rgb;
+  // Store our final texture color
+  vec3 diffuseColor;
+  diffuseColor = texture(u_DiffuseMap, v_texCoord).rgb;
 
-    // (1) Compute ambient light
-    vec3 ambient = pointLights[0].ambientIntensity * pointLights[0].lightColor;
+  // (1) Compute ambient light
+  vec3 ambient = pointLights[0].ambientIntensity * pointLights[0].color;
 
-    // (2) Compute diffuse light
-    // From our lights position and the fragment, we can get
-    // a vector indicating direction
-    // Note it is always good to 'normalize' values.
-    vec3 lightDir = normalize(pointLights[0].lightPos - FragPos);
-    // Now we can compute the diffuse light impact
-    float diffImpact = max(dot(norm, lightDir), 0.0);
-    vec3 diffuseLight = diffImpact * pointLights[0].lightColor;
+  // (2) Compute diffuse light
+  // From our lights position and the fragment, we can get
+  // a vector indicating direction
+  // Note it is always good to 'normalize' values.
+  vec3 lightDir = normalize(pointLights[0].position - FragPos);
+  // Now we can compute the diffuse light impact
+  float diffImpact = max(dot(norm, lightDir), 0.0);
+  vec3 diffuseLight = diffImpact * pointLights[0].color;
 
-    // (3) Compute Specular lighting
-    vec3 viewPos = vec3(0.0,0.0,0.0);
-    vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);
+  // (3) Compute Specular lighting
+  vec3 viewPos = vec3(0.0,0.0,0.0);
+  vec3 viewDir = normalize(viewPos - FragPos);
+  vec3 reflectDir = reflect(-lightDir, norm);
 
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = pointLights[0].specularStrength * spec * pointLights[0].lightColor;
+  float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+  vec3 specular = pointLights[0].specularStrength * spec * pointLights[0].color;
 
-    // Calculate Attenuation here
-    // distance and lighting...
+  // Calculate Attenuation here
+  // distance and lighting...
 
-    // Our final color is now based on the texture.
-    // That is set by the diffuseColor
-    vec3 Lighting = diffuseLight + ambient + specular;
+  // Our final color is now based on the texture.
+  // That is set by the diffuseColor
+  vec3 Lighting = diffuseLight + ambient + specular;
 
-    // Final color + "how dark or light to make fragment"
-    if(gl_FrontFacing){
-        FragColor = vec4(diffuseColor * Lighting,1.0);
-    }else{
-        // Additionally color the back side the same color
-         FragColor = vec4(diffuseColor * Lighting,1.0);
-    }
+  // Final color + "how dark or light to make fragment"
+  if(gl_FrontFacing){
+      FragColor = vec4(diffuseColor * Lighting,1.0);
+  }else{
+      // Additionally color the back side the same color
+       FragColor = vec4(diffuseColor * Lighting,1.0);
+  }
+
 }
