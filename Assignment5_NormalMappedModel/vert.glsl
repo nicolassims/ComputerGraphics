3a78fc3964d21dfe1ded9290e8f5360de5053319
whackney <hackney.w@husky.neu.edu>
2020-04-13 23:04:15 -0400

    updated the vertex shader a bit, no results really

--- Assignment5_NormalMappedModel/vert.glsl
+++ Assignment5_NormalMappedModel/vert.glsl
@@ -1,9 +1,7 @@
 #version 330
-layout(location = 0) in vec3 position;
-layout(location = 1) in vec3 normal;
-layout(location = 2) in vec2 textureCoords;
-layout(location = 3)in vec3 tangents; // Our third attribute - texture coordinates.
-layout(location = 4)in vec3 bitangents; // Our third attribute - texture coordinates.
+layout(location = 1) in vec3 position;
+layout(location = 0) in vec2 textureCoords;
+layout(location = 2) in vec3 normal;
 
 // We now have our camera system set up.
 uniform mat4 modelMatrix;
